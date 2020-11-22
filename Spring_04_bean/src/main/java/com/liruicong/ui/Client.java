1a2ea2b7d04b4657dfe563f182de8311c003c90e
Ellery-Lee <ruicong.li@berkeley.edu>
2020-11-22 11:24:53 +0800

    update 11.22

--- Spring_04_bean/src/main/java/com/liruicong/ui/Client.java
+++ Spring_04_bean/src/main/java/com/liruicong/ui/Client.java
@@ -16,9 +16,13 @@ public class Client {
      */
     public static void main(String[] args) {
         //1.获取核心容器对象
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2.根据id获取Bean对象
         IAccountService as = (IAccountService)ac.getBean("accountService");
         as.saveAccount();
+
+        //手动关闭容器
+        ac.close();
     }
 }
