82eccac18ef8eb60217f01738b5e02940b547542
Ellery-Lee <ruicong.li@berkeley.edu>
2020-11-23 21:06:01 +0800

    update 11.23

---
+++ Spring_07_account_xmlioc/src/main/java/com/liruicong/domain/Account.java
@@ -0,0 +1,45 @@
+package com.liruicong.domain;
+
+import java.io.Serializable;
+
+/**
+ * 账户的实体类
+ */
+public class Account implements Serializable {
+    private Integer id;
+    private String username;
+    private Float money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
