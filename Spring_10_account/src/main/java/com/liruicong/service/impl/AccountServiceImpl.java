69364eaaccca63f9d3fa1e9f022ea91dad7f45db
Ellery-Lee <ruicong.li@berkeley.edu>
2020-12-02 15:47:29 +0800

    update 12.2

--- Spring_10_account/src/main/java/com/liruicong/service/impl/AccountServiceImpl.java
+++ Spring_10_account/src/main/java/com/liruicong/service/impl/AccountServiceImpl.java
@@ -14,136 +14,48 @@ import java.util.List;
  */
 public class AccountServiceImpl implements IAccountService {
     private IAccountDao accountDao;
-    private TransactionManager txManager;
-
-    public void setTxManager(TransactionManager txManager) {
-        this.txManager = txManager;
-    }
 
     public void setAccountDao(IAccountDao accountDao) {
         this.accountDao = accountDao;
     }
 
     public List<Account> findAllAccount() {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            List<Account> accounts = accountDao.findAllAccount();
-            //3、提交事务
-            txManager.commit();
-            //4、返回结果
-            return accounts;
-        }catch (Exception e){
-            //5、回滚操作
-            txManager.rollback();
-            throw new RuntimeException(e);
-        }finally {
-            //6、释放连接
-            txManager.release();
-        }
+        return accountDao.findAllAccount();
     }
 
     public Account findAccountById(Integer accountId) {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            Account account = accountDao.findAccountById(accountId);
-            //3、提交事务
-            txManager.commit();
-            //4、返回结果
-            return account;
-        }catch (Exception e){
-            //5、回滚操作
-            txManager.rollback();
-            throw new RuntimeException(e);
-        }finally {
-            //6、释放连接
-            txManager.release();
-        }
+        return accountDao.findAccountById(accountId);
     }
 
     public void saveAccount(Account account) {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            accountDao.saveAccount(account);
-            //3、提交事务
-            txManager.commit();
-        }catch (Exception e){
-            //4、回滚操作
-            txManager.rollback();
-        }finally {
-            //5、释放连接
-            txManager.release();
-        }
+        accountDao.saveAccount(account);
     }
 
     public void updateAccount(Account account) {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            accountDao.updateAccount(account);
-            //3、提交事务
-            txManager.commit();
-        }catch (Exception e){
-            //4、回滚操作
-            txManager.rollback();
-        }finally {
-            //5、释放连接
-            txManager.release();
-        }
+        accountDao.updateAccount(account);
     }
 
     public void deleteAccount(Integer accountId) {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            accountDao.deleteAccount(accountId);
-            //3、提交事务
-            txManager.commit();
-        }catch (Exception e){
-            //4、回滚操作
-            txManager.rollback();
-        }finally {
-            //5、释放连接
-            txManager.release();
-        }
+        accountDao.deleteAccount(accountId);
     }
 
     public void transfer(String sourceName, String targetName, Float money) {
-        try{
-            //1、开启事务
-            txManager.beginTransaction();
-            //2、执行操作
-            /*
-            问题：每一个数据库操作都算作一次连接，没有把所有操作打包成事务执行。
-            需要使用ThreadLocal对象把Connection和当前线程绑定，从而使一个线程中只有一个能控制事务的对象
-            */
-            //2.1、根据名称查询转出账户
-            Account source = accountDao.findAccountByName(sourceName);
-            //2.2、根据名称查询转入账户
-            Account target = accountDao.findAccountByName(targetName);
-            //2.3、转出账户减钱
-            source.setMoney(source.getMoney() - money);
-            //2.4、转入账户加钱
-            target.setMoney(target.getMoney() + money);
-            //2.5、更新转出账户
-            accountDao.updateAccount(source);
-            //2.6、更新转入账户
-            accountDao.updateAccount(target);
-            //3、提交事务
-            txManager.commit();
-        }catch (Exception e){
-            //4、回滚操作
-            txManager.rollback();
-        }finally {
-            //5、释放连接
-            txManager.release();
-        }
+        System.out.println("transfer...");
+        /*
+        问题：每一个数据库操作都算作一次连接，没有把所有操作打包成事务执行。
+        需要使用ThreadLocal对象把Connection和当前线程绑定，从而使一个线程中只有一个能控制事务的对象
+        */
+        //2.1、根据名称查询转出账户
+        Account source = accountDao.findAccountByName(sourceName);
+        //2.2、根据名称查询转入账户
+        Account target = accountDao.findAccountByName(targetName);
+        //2.3、转出账户减钱
+        source.setMoney(source.getMoney() - money);
+        //2.4、转入账户加钱
+        target.setMoney(target.getMoney() + money);
+        //2.5、更新转出账户
+        accountDao.updateAccount(source);
+        //2.6、更新转入账户
+        accountDao.updateAccount(target);
     }
 }
