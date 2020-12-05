fda87b3c9defd5c9e95aa51cb3b945553b6fd4f2
Ellery-Lee <ruicong.li@berkeley.edu>
2020-12-05 15:41:53 +0800

    update 12.5"

---
+++ Spring_19_tx_xml/src/main/java/com/liruicong/jdbctemplate/JdbcTemplateDemo1.java
@@ -0,0 +1,25 @@
+package com.liruicong.jdbctemplate;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * JdbcTemplate的最基本用法
+ */
+public class JdbcTemplateDemo1 {
+    public static void main(String[] args) {
+        //准备数据源：Spring的内置数据源
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/springstudy?serverTimezone=GMT");
+        ds.setUsername("root");
+        ds.setPassword("Li19980503");
+
+        //1、创建JdbcTemplate对象
+        JdbcTemplate jt = new JdbcTemplate();
+        //给jt设置数据源
+        jt.setDataSource(ds);
+        //2、执行操作
+        jt.execute("insert into account (username, money)values ('ddd', 1000)");
+    }
+}
