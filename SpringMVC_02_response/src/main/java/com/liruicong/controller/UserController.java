94c26b5b6674829c76512358f879c1ef6e962df9
Ellery-Lee <ruicong.li@berkeley.edu>
2020-12-08 20:20:18 +0800

    update 12.8

--- SpringMVC_02_response/src/main/java/com/liruicong/controller/UserController.java
+++ SpringMVC_02_response/src/main/java/com/liruicong/controller/UserController.java
@@ -4,6 +4,7 @@ import com.liruicong.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -55,5 +56,43 @@ public class UserController {
         //直接会进行相应
         response.getWriter().println("你好");
         return;
+    }
+
+    /**
+     * 返回ModelAndView
+     * @return
+     */
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView(){
+        //创建ModelAndView对象
+        ModelAndView mv = new ModelAndView();
+        System.out.println("testModelAndView方法执行了...");
+        //模拟从数据库中查询出User对象
+        User user = new User();
+        user.setUsername("小凤");
+        user.setPassword("456");
+        user.setAge(30);
+
+        //把user对象存储到mv对象中，也会把user对象存入到request对象
+        mv.addObject("user", user);
+
+        //跳转到哪个页面
+        mv.setViewName("success");
+        return mv;
+    }
+
+    /**
+     * 使用关键字的方式进行转发或者重定向
+     * @return
+     */
+    @RequestMapping("/testForwardOrRedirect")
+    public String testForwardOrRedirect(){
+        System.out.println("testForwardOrRedirect方法执行了...");
+        //请求的转发
+        //return "forward:/WEB-INF/pages/success.jsp";
+
+
+        //重定向
+        return "redirect:/index.jsp";
     }
 }
--- SpringMVC_02_response/src/main/webapp/response.jsp
+++ SpringMVC_02_response/src/main/webapp/response.jsp
@@ -9,6 +9,15 @@
 <html>
 <head>
     <title>Title</title>
+    <script src="js/jquery.min.js"></script>
+    <script>
+        //页面加载，绑定单击事件
+        $((function){
+           $("#btn").click(function(){
+                alert("hello btn");
+           });
+        });
+    </script>
 </head>
 <body>
     <a href="user/testString">testString</a>
@@ -16,5 +25,17 @@
     <br/>
 
     <a href="user/testVoid">testVoid</a>
+
+    <br/>
+
+    <a href="user/testModelAndView">testModelAndView</a>
+
+    <br/>
+
+    <a href="user/testForwardOrRedirect">testForwardOrRedirect</a>
+
+    <br/>
+
+    <button id="btn">发送ajax的请求</button>
 </body>
 </html>
