092844c54785931f1db8b416280d19141a2a8018
Ellery-Lee <ruicong.li@berkeley.edu>
2020-12-11 11:01:51 +0800

    update 12.11

---
+++ Gradle_02_Todo/src/main/java/com/liruicong/todo/TodoItem.java
@@ -0,0 +1,36 @@
+package com.liruicong.todo;
+
+public class TodoItem {
+    //待办事项的名称
+    private String name;
+    //已完成
+    private boolean hasDone;
+
+    public TodoItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isHasDone() {
+        return hasDone;
+    }
+
+    public void setHasDone(boolean hasDone) {
+        this.hasDone = hasDone;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItem{" +
+                "name='" + name + '\'' +
+                ", hasDone=" + hasDone +
+                '}';
+    }
+}
